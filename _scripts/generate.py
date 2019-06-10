a4da45bf60e1620102f9d9fa1a66787378e36397
Abbe98 <albin.post@gmail.com>
2019-06-10 18:35:25 +0200

    update script for new content

--- _scripts/generate.py
+++ _scripts/generate.py
@@ -3,7 +3,7 @@ from pywikibot import Site, Category
 import json
 
 site = Site('commons', 'commons')
-cat = Category(site, 'Category:Images_from_Nordiska_museet/Fashion_plates')
+cat = Category(site, 'Category:Images_from_Nordiska_museet:_2019-06')
 
 pages = list()
 for page in CategorizedPageGenerator(cat, recurse=False, namespaces=6):
@@ -11,6 +11,8 @@ for page in CategorizedPageGenerator(cat, recurse=False, namespaces=6):
     item['id'] = page.pageid
     print(page.title())
     item['title'] = str(page.title())
+    if (('(2)' in item['title']) or ('(3)' in item['title'])):
+        continue
     pages.append(item)
 
     with open('../static/pages.json', 'w') as outfile:
